c7e2d49b879b48d701535f6b961a410e94a43af3
keijiro tanabe <ayt7gysnyx@gmail.com>
2017-06-05 21:58:39 +0900

    add profile page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/about', function(req, res) {
 	res.sendFile(__dirname + '/about.html');
 });
 
+app.get('/profile', function(req, res) {
+	res.sendFile(__dirname + '/profile.html');
+});
+
 http.listen(process.env.PORT || 3000, function() {
 	console.log('Server listening on port 3000');
 });
